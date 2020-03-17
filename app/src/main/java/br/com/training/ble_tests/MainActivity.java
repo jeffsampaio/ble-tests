5eba4b4ed251ea6566b7cd1921cb1c554c0433be
jeffsampaio <jeffersonsdm03@gmail.com>
2020-03-17 16:18:49 -0300

    Adding BluetoothLeService from official connectivity samples

--- app/src/main/java/br/com/training/ble_tests/MainActivity.java
+++ app/src/main/java/br/com/training/ble_tests/MainActivity.java
@@ -1,59 +1,136 @@
 package br.com.training.ble_tests;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int REQUEST_ENABLE_BT = 4;
-    private static int STATE_CONNECTED = 1;
+    private static String ET_SENSOR_ADDRESS = "1C:87:74:01:73:10";
 
-    private static String HT_SENSOR_ADDRESS = "00001809-0000-1000-8000-00805f9b34fb";
+    private static UUID EAR_THERMOMETER_SERVICE_UUID = convertFromInteger(0x1809);
+    private static UUID EAR_THERMOMETER_MEASUREMENT_CHAR_UUID = convertFromInteger(0x2A1C);
+    private static UUID CLIENT_CHARACTERISTIC_CONFIG_UUID = convertFromInteger(0x2902);
 
-    private UUID HEART_THERMOMETER_SERVICE_UUID = convertFromInteger(0x180D);
-    private UUID HEART_THERMOMETER_MEASUREMENT_CHAR_UUID = convertFromInteger(0x2A37);
-    private UUID HEART_THERMOMETER_CONTROL_POINT_CHAR_UUID = convertFromInteger(0x2A39);
+    private static int REQUEST_ENABLE_BT = 1;
+    private static final long SCAN_PERIOD = 10000;  // Stops scanning after 10 seconds.
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private boolean mScanning;
+    private Handler mHandler;
+    private List<String> mDevicesAdded;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BluetoothAdapter bluetoothAdapter;
+        mHandler = new Handler();
+        mDevicesAdded = new ArrayList<>();
 
-        final BluetoothManager bluetoothManager =
-                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-
-        bluetoothAdapter = bluetoothManager.getAdapter();
-
-        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
-            startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), REQUEST_ENABLE_BT);
+        // Determine whether BLE is supported on the device.
+        if (!(getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE))) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
         }
 
-        BluetoothAdapter.LeScanCallback scanCallback = new BluetoothAdapter.LeScanCallback() {
-            @Override
-            public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+        // Initializes Bluetooth adapter.
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
 
-            }
-        };
-
-        bluetoothAdapter.startLeScan(scanCallback);
+        // Checks if Bluetooth is supported on the device.
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
     }
 
-    public UUID convertFromInteger(int i) {
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Ensures Bluetooth is enabled on the device.  If Bluetooth is not currently enabled,
+        // fire an intent to display a dialog asking the user to grant permission to enable it.
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        scanLeDevice(true);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        // User choose not to enable Bluetooth.
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
+            finish();
+            return;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        scanLeDevice(false);
+    }
+
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                }
+            }, SCAN_PERIOD);
+
+            mScanning = true;
+            mBluetoothAdapter.startLeScan(mLeScanCallback);
+        } else {
+            mScanning = false;
+            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        }
+    }
+
+    private BluetoothAdapter.LeScanCallback mLeScanCallback =
+            new BluetoothAdapter.LeScanCallback() {
+
+        @Override
+        public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mDevicesAdded.add(device.getAddress());
+                }
+            });
+        }
+    };
+
+    public static UUID convertFromInteger(int i) {
         final long MSB = 0x0000000000001000L;
         final long LSB = 0x800000805f9b34fbL;
-
         long value = i & 0xFFFFFFFF;
-
         return new UUID(MSB | (value << 32), LSB);
     }
+
+
 }
